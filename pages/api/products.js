50aa70c9ab7dd78aaa366cbc5cf91312f0e83a13
Rabee <rabeerd@gmail.com>
2020-01-07 16:50:50 +0100

    Week 1 - Using MongoDB with Atlas

---
+++ pages/api/products.js
@@ -0,0 +1,11 @@
+// import products from '../../static/products';
+import Product from '../../models/Product'
+import connectDb from '../../utils/connectDb';
+
+connectDb();
+
+export default async (req, res) => {
+  const products = await Product.find()
+  res.status(200).json(products)
+
+}
