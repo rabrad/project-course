ac9a3d506b3c4ff7630d953ba499d8d83348e995
Rabee <rabeerd@gmail.com>
2020-01-19 15:53:57 +0100

    implement search bar with react semantic

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import ProductList from "../components/Index/ProductList";
 import ProductPagination from "../components/Index/ProductPagination";
 import baseUrl from "../utils/baseUrl";
+import { Search, Grid, Header, Segment } from 'semantic-ui-react';
+import _ from 'lodash'
+
+
+
+
+
 
 function Home({ products, totalPages }) {
+  const [isLoading, setLoading] = useState(false);
+  const [results, setResults] = useState([]);
+  const [value, setValue] = useState('');
+
+  // const source = products.reduce((accumulator, currentValue) => {
+
+  //   title: product.name;
+  //   price: product.price;
+  //   mediaUrl: product.mediaUrl
+  // }, []
+
+
+
+
+  // )
+
+  const handleResultSelect = (e, { result }) => setValue(result.name);
+
+  const handleSearchChange = (e, { value }) => {
+    setLoading(true);
+    setValue(value);
+
+    setTimeout(() => {
+      if (value.length < 1) {
+        setLoading(false);
+        setResults([])
+        setValue('');
+
+        return;
+      }
+
+      const re = new RegExp(_.escapeRegExp(value), 'i')
+      const isMatch = (result) => re.test(result.name)
+
+      setLoading(false);
+      setResults(_.filter(products, isMatch))
+
+    }, 300)
+  }
+
+
   return (
     <>
+      <Grid>
+        <Grid.Column width={6}>
+          <Search
+            loading={isLoading}
+            onResultSelect={(e, { result }) => handleResultSelect(e, { result })}
+            onSearchChange={_.debounce(handleSearchChange, 500, {
+              leading: true,
+            })}
+            results={results}
+            value={value}
+          />
+        </Grid.Column>
+      </Grid>
+
       <ProductList products={products} />
       <ProductPagination totalPages={totalPages} />
     </>
